a94a2521308f926b32f136e2846a02dd737a8647
Royal6969 <sergiodiazcampos@gmail.com>
2021-08-07 23:43:02 +0200

    choose address on google maps (udemy)

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ import SearchScreen from './screens/SearchScreen';
 import { listProductCategories } from './actions/productActions';
 import LoadingBox from './components/LoadingBox';
 import MessageBox from './components/MessageBox';
+import MapScreen from './screens/MapScreen';
 
 function App() {
 
@@ -191,6 +192,10 @@ return (
                 <PrivateRoute 
                     path="/profile" 
                     component={ProfileScreen}
+                ></PrivateRoute>
+                <PrivateRoute 
+                    path="/map" 
+                    component={MapScreen}
                 ></PrivateRoute>
                 <AdminRoute 
                     path="/productlist" 
